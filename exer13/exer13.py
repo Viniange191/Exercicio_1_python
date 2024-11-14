68266674a65b715a42cf6d55d83badfc9aa37f5a
Viniange191 <viniangelsalves@gmail.com.br>
2024-11-14 10:23:00 -0300

    Meus primeiros exercicios de python

---
+++ exer13/exer13.py
@@ -0,0 +1,4 @@
+for numero in range(1, 101):
+    #Se o numero for divido por 2 e o resto não for 0, ele entra
+    if numero % 2 != 0:
+        print(numero,end= '')
---
+++ exer14/exer14.py
@@ -0,0 +1,16 @@
+numeros = []
+for i in range(10):
+    try:
+        #armazena o elemento digitado
+        numero = int(input('Digite um número inteiro: '))
+        print('(',i+1,'-','10)')
+        numeros.append(numero)
+    #Se o valor for incorreto
+    except ValueError:
+        print('Entrada inválida!!!')
+    #Função SUM: soma automaticamente todos os numeros dentro dos elementos
+soma = sum(numeros)
+    #Calcula a média
+media = soma / len(numeros)
+print('Soma:', soma)
+print('Média:', media)
